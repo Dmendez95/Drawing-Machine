f7ac5e4116751d3a7fd8a3261fb3cb0c369db98e
Dmendez95 <83195637+Dmendez95@users.noreply.github.com>
2021-11-16 01:20:02 -0800

    noise 2.0

--- sketch.js
+++ sketch.js
@@ -1,28 +1,31 @@
 let noiseOffset = 0.0;
+let strokeWidth = 5;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background(220, 50, 133);
 
-  strokeWeight(5);
+
 
 }
 
 function draw() {
 
-    background(220, 50, 133, 5);
+  background(220, 50, 133, 5);
+  strokeWeight(strokeWidth);
 
-noiseOffset = noiseOffset + 0.01;
+  noiseOffset += 0.05;
+  strokeWidth = noise(noiseOffset) * 100;
 
-    stroke(map(mouseX, 0, 600, 0, 255, true))
-    line(width - mouseX, height - mouseY, width - pmouseX, height - pmouseY);
-    line(mouseX, mouseY, pmouseX, pmouseY);
-  }
+  stroke(map(mouseX, 0, 600, 0, 255, true))
+  line(width - mouseX, height - mouseY, width - pmouseX, height - pmouseY);
+  line(mouseX, mouseY, pmouseX, pmouseY);
+}
 
 
-function keyTyped(){
+function keyTyped() {
 
-  if (key === 's'){
+  if (key === 's') {
     // save this image
     saveCanvas('fileName', 'png');
   } else if (key === 'c') {
